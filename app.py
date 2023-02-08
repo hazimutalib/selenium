8f7231785bbb22bb78d70713d851caf44613eb85
HAZIM MUTALIB <hazimutalib96@gmail.com>
2023-02-08 16:09:28 +0800

    update date

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ st.download_button(
   mime='text/csv',
 )
 
-
+st.write(df.groupby('posted_date')[['comment']].count())
 
 line_chart_data = pd.DataFrame(df.groupby('posted_date')[['comment']].count())
 
